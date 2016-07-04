c5b65bd29ac72f9cb6c0af0c5620cc91177db75d
Andrea Bonandin <andrea.bonandin01@universitadipavia.it>
2016-07-04 15:59:42 +0200

    silenced components when robot has score = 0

--- test_toml/score.py
+++ test_toml/score.py
@@ -33,8 +33,9 @@ MORSELABPATH = os.environ.get("MORSELABPATH")
 GAMESPATH = os.path.join(MORSELABPATH, "games")
 
 if __name__ == '__main__':
-	startTime = time.time()
-	maxScore = 100
+	with pymorse.Morse() as simu:
+		startTime = simu.time()
+		maxScore = 100
 
 	with pymorse.Morse() as simu:
 		robots = {}
@@ -42,40 +43,46 @@ if __name__ == '__main__':
 			robots[x] = {}
 			robots[x]['score'] = maxScore
 
-	# Open and load the local file containing the configuration of the simulation
-	with open(os.path.join(PWD, "g.toml"), 'r') as gfile:
-		simulation = toml.loads(gfile.read())
+		# Open and load the local file containing the configuration of the simulation
+		with open(os.path.join(PWD, "g.toml"), 'r') as gfile:
+			simulation = toml.loads(gfile.read())
 
-		#Check if game file exists and load it
-		game_name = simulation['game']['name']
-		try:
-			game_name = findFileInPath(game_name, 'toml', [PWD, GAMESPATH])
-		except:
-			raise
+			#Check if game file exists and load it
+			game_name = simulation['game']['name']
+			try:
+				game_name = findFileInPath(game_name, 'toml', [PWD, GAMESPATH])
+			except:
+				raise
 
-	with open(game_name) as gamefile:
-		game = toml.loads(gamefile.read())
+		with open(game_name) as gamefile:
+			game = toml.loads(gamefile.read())
 
-	for timeGame in game['game']['time']:
-		timeSimu = timeGame['timeSimu']
-		scoreZero = timeGame['scoreZero']
-		endTime = timeGame['endTime']
+		for timeGame in game['game']['time']:
+			timeSimu = timeGame['timeSimu']
+			scoreZero = timeGame['scoreZero']
+			endTime = timeGame['endTime']
 
-	if len(sys.argv) == 4:
-		timeSimu = strToInt(sys.argv[1])
-		endTime = strToBool(sys.argv[2])
-		scoreZero = strToBool(sys.argv[3])
-	if len(sys.argv) != 1 and len(sys.argv) != 4:
-		raise Exception('Wrong number of parameters')
+		if len(sys.argv) == 4:
+			timeSimu = strToInt(sys.argv[1])
+			endTime = strToBool(sys.argv[2])
+			scoreZero = strToBool(sys.argv[3])
+		if len(sys.argv) != 1 and len(sys.argv) != 4:
+			raise Exception('Wrong number of parameters')
 
-	loss = maxScore/timeSimu
-	while not ((endTime and timeSimu <= 0) or (scoreZero and max([i['score'] for i in robots.values()]) <= 0)):
-		print('\033[H\033[2J')
-		for x in robots.keys():
-			robots[x]['score'] -= loss
-			robots[x]['score'] = max(robots[x]['score'], 0)
-			print("Robot '{}' score = {}".format(x, robots[x]['score']))
-			#sys.stdout.write("Robot '{}' score = {}".format(x, robots[x]['score']))
-			#sys.stdout.flush()
-		time.sleep(0.1)
-		timeSimu = timeSimu - (time.time() - startTime)
+		loss = maxScore/timeSimu
+		while not ((endTime and timeSimu <= 0) or (scoreZero and max([i['score'] for i in robots.values()]) <= 0)):
+			print('\033[H\033[2J')
+			for x in robots.keys():
+				robots[x]['score'] -= loss
+				robots[x]['score'] = max(robots[x]['score'], 0)
+				if robots[x]['score'] == 0:
+					components = simu.__dict__[x]
+					for c in components:
+						s = x + "." + c
+						try:
+							simu.deactivate(s)
+						except pymorse.MorseServiceFailed:
+							pass
+				print("Robot '{}' score = {}".format(x, robots[x]['score']))
+			time.sleep(0.2)
+			timeSimu = timeSimu - (simu.time() - startTime)
