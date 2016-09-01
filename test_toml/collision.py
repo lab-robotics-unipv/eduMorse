d790582ea26fbc12e38228b32d685fd311ff417c
Andrea Bonandin <andrea.bonandin01@universitadipavia.it>
2016-09-01 17:47:21 +0200

    added layer and its configuration, bound with collision and score

--- test_toml/collision.py
+++ test_toml/collision.py
@@ -1,37 +1,56 @@
 import pymorse
+import socket
 
 period = 1/3
+HOST = ''
+PORT = 50000
 
-def counter(data, x, robots):
+def send(robot, obj, socket):
+	message = robot + '.' + obj + '\x04'
+	print(message)
+	socket.sendall(message.encode('utf-8'))
+
+
+def counter(data, x, robots, socket):
 
 	if data["collision"]: # collision occurred
-		if robots[x]['obj'] == data["objects"]: # robot collides again with the same object of the previous collision
+		for o in data["objects"].split(','):
+			if robots[x]['obj'] == o: # robot collides again with the same object of the previous collision
 
-			# check if robot is still colliding against the object considering the frequency of the sensor
-			if (data['timestamp'] - robots[x]['timestamp']) > 1.2*period: # 1.2=factor of security for float count
+				# check if robot is still colliding against the object considering the frequency of the sensor
+				if (data['timestamp'] - robots[x]['timestamp']) > 1.2*period: # 1.2=factor of security for float count
+					robots[x]['nb_collisions'] += 1
+					print("Collision with {}! In total, {} collisions occured, robot name '{}'".format(robots[x]['obj'], robots[x]['nb_collisions'], x))
+					send(x, robots[x]['obj'], socket)
+
+			else:
+				# robot collides with a different object than the previous collision
 				robots[x]['nb_collisions'] += 1
+				robots[x]['obj'] = o
 				print("Collision with {}! In total, {} collisions occured, robot name '{}'".format(robots[x]['obj'], robots[x]['nb_collisions'], x))
+				send(x, robots[x]['obj'], socket)
 
-		else:
-			# robot collides with a different object than the previous collision
-			robots[x]['nb_collisions'] += 1
-			robots[x]['obj'] = data["objects"]
-			print("Collision with {}! In total, {} collisions occured, robot name '{}'".format(robots[x]['obj'], robots[x]['nb_collisions'], x))
-
-		robots[x]['timestamp'] = data["timestamp"] # timestamp is registered every time
+			robots[x]['timestamp'] = data["timestamp"] # timestamp is registered every time
 
 
-with pymorse.Morse() as simu:
-	robots = {}
-	for x in simu.robots:
-		robots[x] = {}
-		robots[x]['timestamp'] = 0
-		robots[x]['nb_collisions'] = 0
-		robots[x]['obj'] = None
+if __name__ == '__main__':
+	with pymorse.Morse() as simu:
+		robots = {}
+		for x in simu.robots:
+			robots[x] = {}
+			robots[x]['timestamp'] = 0
+			robots[x]['nb_collisions'] = 0
+			robots[x]['obj'] = None
 
-	for x in robots.keys():
-		simu.__dict__[x].collision.subscribe(lambda data: counter(data, x, robots))
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+			s.bind((HOST, PORT))
+			s.listen(1)
+			conn, addr = s.accept()
 
-	print("Press ctrl+C to stop")
-	while True:
-		simu.sleep(10)
+			for x in robots.keys():
+				simu.__dict__[x].collision.subscribe(lambda data: counter(data, x, robots, conn))
+
+			print("Press ctrl+C to stop")
+			while True:
+				s.close()
+				simu.sleep(10)
--- test_toml/score.py
+++ test_toml/score.py
@@ -1,8 +1,11 @@
 import pymorse
 import pytoml as toml
 import os
+import select
+import socket
 import sys
 import time
+
 
 # control if a file exists
 def findFileInPath(filename, extension, paths):
@@ -13,12 +16,14 @@ def findFileInPath(filename, extension, paths):
 			return os.path.join(path, filename + '.' + extension)
 	raise FileNotFoundError('File ' + filename + ' not found')
 
+
 def strToBool(s):
 	if s == 'True':
 		return True
 	if s == 'False':
 		return False
 	raise ValueError('Wrong input')
+
 
 def strToInt(s):
 	try:
@@ -28,66 +33,100 @@ def strToInt(s):
 		raise
 	return i
 
+
+def receive(conn):
+	data = b''
+	word = b''
+	while word != b'\x04':
+		data += word
+		word = conn.recv(1)
+	message = data.decode('utf-8')
+	return message
+
+
+def messageInSocket(s):
+	read_list, _, _ = select.select([s], [], [], 0)
+	if read_list == []:
+		return False
+	else:
+		return True
+
+
+HOST = 'localhost'
+PORT = 50001
 PWD = os.path.join(os.environ['HOME'], 'simulator/test_toml')
 MORSELABPATH = os.environ.get("MORSELABPATH")
 GAMESPATH = os.path.join(MORSELABPATH, "games")
+TOMLPATH = os.path.join(MORSELABPATH, "test_toml")
 
 if __name__ == '__main__':
 	with pymorse.Morse() as simu:
-		startTime = time.time()
-		maxScore = 100
 
-		robots = {}
-		for x in simu.robots:
-			robots[x] = {}
-			robots[x]['score'] = maxScore
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+			s.connect((HOST, PORT))
 
-		# Open and load the local file containing the configuration of the simulation
-		with open(os.path.join(PWD, "g.toml"), 'r') as gfile:
-			simulation = toml.loads(gfile.read())
+			startTime = time.time()
+			maxScore = 100
 
-			#Check if game file exists and load it
-			game_name = simulation['game']['name']
-			try:
-				game_name = findFileInPath(game_name, 'toml', [PWD, GAMESPATH])
-			except:
-				raise
+			robots = {}
+			for x in simu.robots:
+				robots[x] = {}
+				robots[x]['score'] = maxScore
 
-		with open(game_name) as gamefile:
-			game = toml.loads(gamefile.read())
+			# Open and load the local file containing the configuration of the simulation
+			with open(os.path.join(PWD, "g.toml"), 'r') as gfile:
+				simulation = toml.loads(gfile.read())
 
-		for timeGame in game['game']['time']:
-			timeSimu = timeGame['timeSimu']
-			scoreZero = timeGame['scoreZero']
-			endTime = timeGame['endTime']
+				#Check if game file exists and load it
+				game_name = simulation['game']['name']
+				try:
+					game_name = findFileInPath(game_name, 'toml', [PWD, GAMESPATH])
+				except:
+					raise
 
-		if len(sys.argv) == 4:
-			timeSimu = strToInt(sys.argv[1])
-			endTime = strToBool(sys.argv[2])
-			scoreZero = strToBool(sys.argv[3])
-		if len(sys.argv) != 1 and len(sys.argv) != 4:
-			raise Exception('Wrong number of parameters')
+			with open(game_name) as gamefile:
+				game = toml.loads(gamefile.read())
 
-		loss = maxScore/timeSimu
-		timeLeft = 1
-		while not ((endTime and timeLeft <= 0) or (scoreZero and max([i['score'] for i in robots.values()]) <= 0)):
-			print('\033[H\033[2J')
-			diffStartTime = time.time() - startTime
-			timeLeft = timeSimu - diffStartTime
-			print('{}| {}'.format('Robot'.center(20), 'Score'.center(20)))
-			print('----------------------------------------------')
-			for x in robots.keys():
-				robots[x]['score'] = maxScore - loss*diffStartTime
-				robots[x]['score'] = max(robots[x]['score'], 0)
-				if robots[x]['score'] == 0:
-					components = simu.__dict__[x]
-					for c in components:
-						s = x + "." + c
-						try:
-							simu.deactivate(s)
-						except pymorse.MorseServiceFailed:
-							pass
-				print('{}| {}'.format(x.center(20), str(round(robots[x]['score'], 1)).center(20)))
-			print('----------------------------------------------')
-			print('{}:{}'.format('Time'.center(5), str(int(timeSimu - timeLeft)).center(5)))
-			time.sleep(0.2)
+			for timeGame in game['game']['time']:
+				timeSimu = timeGame['timeSimu']
+				scoreZero = timeGame['scoreZero']
+				endTime = timeGame['endTime']
+
+			if len(sys.argv) == 4:
+				timeSimu = strToInt(sys.argv[1])
+				endTime = strToBool(sys.argv[2])
+				scoreZero = strToBool(sys.argv[3])
+			if len(sys.argv) != 1 and len(sys.argv) != 4:
+				raise Exception('Wrong number of parameters')
+
+			loss = maxScore/timeSimu
+			timeLeft = 1
+			while not ((endTime and timeLeft <= 0) or (scoreZero and max([i['score'] for i in robots.values()]) <= 0)):
+				print('\033[H\033[2J')
+				diffStartTime = time.time() - startTime
+				timeLeft = timeSimu - diffStartTime
+				while messageInSocket(s):
+					message = receive(s)
+					point = message.find('.')
+					robot = message[:point]
+					score = message[point + 1:]
+					for x in robots.keys():
+						if x == robot:
+							robots[x]['score'] += float(score)
+				print('{}| {}'.format('Robot'.center(20), 'Score'.center(20)))
+				print('----------------------------------------------')
+				for x in robots.keys():
+					robots[x]['score'] = maxScore - loss*diffStartTime
+					robots[x]['score'] = max(robots[x]['score'], 0)
+					if robots[x]['score'] == 0:
+						components = simu.__dict__[x]
+						for c in components:
+							s = x + "." + c
+							try:
+								simu.deactivate(s)
+							except pymorse.MorseServiceFailed:
+								pass
+					print('{}| {}'.format(x.center(20), str(round(robots[x]['score'], 1)).center(20)))
+				print('----------------------------------------------')
+				print('{}:{}'.format('Time'.center(5), str(int(diffStartTime)).center(5)))
+				time.sleep(0.2)
