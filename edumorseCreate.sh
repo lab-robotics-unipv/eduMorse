3070b10840225a894d10b17cf30a81d64fb547d5
Andrea Bonandin <andrea.bonandin01@universitadipavia.it>
2016-09-02 16:26:39 +0200

    bash script to create new projects with test_toml folder

---
+++ edumorseCreate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+	echo "Name of project not found"
+else
+	morse create $1
+	cd $1
+	rm default.py
+	rm -r scripts/
+	rm -r src/
+	cp -r /opt/morseLab/test_toml/* .
+fi
