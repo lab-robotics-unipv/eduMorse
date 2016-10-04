fc5e1568810e2aee3692e326620e6fa605420197
Daniele De Martini <diemme89@gmail.com>
2016-10-04 15:29:02 -0300

    changed robot.py into library

---
+++ socket/socketComm.py
@@ -0,0 +1,52 @@
+import select
+import socket
+
+
+def receive(conn):
+    data = b''
+    word = b''
+    while word != b'\x04':
+        data += word
+        word = conn.recv(1)
+    message = data.decode('utf-8')
+    return message
+
+
+def send(stringa, socket):
+    message = stringa + '\x04'
+    socket.sendall(message.encode('utf-8'))
+
+
+def messageInSocket(s):
+    read_list, _, _ = select.select([s], [], [], 0)
+    if read_list == []:
+        return False
+    else:
+        return True
+
+
+if __name__ == '__main__':
+    robot_sender = 'robot'
+    robot_receiver = 'robot2'
+    HOST = 'localhost'
+    PORT = 4001
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        send(robot_sender, s)
+        start = ''
+        while 'Start' not in start:
+            start = receive(s)
+            print(start)
+
+        message = robot_receiver + '{ciao}'
+        send(message, s)
+        message = robot_receiver + '{ciao}'
+        send(message, s)
+
+        while not messageInSocket(s):
+            pass
+        data = receive(s)
+        print(data)
+
+        s.close()
