05a66b99f22923d5722659cf67e11c58b3cb5e28
Daniele De Martini <diemme89@gmail.com>
2017-01-11 11:21:02 -0400

    socket scripts

--- eduMorse/socket/robot2.py
+++ eduMorse/socket/robot2.py
@@ -2,26 +2,26 @@ import select
 import socket
 
 def receive(conn):
-	data = b''
-	word = b''
-	while word != b'\x04':
-		data += word
-		word = conn.recv(1)
-	message = data.decode('utf-8')
-	return message
+    data = b''
+    word = b''
+    while word != b'\x04':
+        data += word
+        word = conn.recv(1)
+    message = data.decode('utf-8')
+    return message
 
 
 def send(stringa, socket):
-	message = stringa + '\x04'
-	socket.sendall(message.encode('utf-8'))
+    message = stringa + '\x04'
+    socket.sendall(message.encode('utf-8'))
 
 
 def messageInSocket(s):
-	read_list, _, _ = select.select([s], [], [], 0)
-	if read_list == []:
-		return False
-	else:
-		return True
+    read_list, _, _ = select.select([s], [], [], 0)
+    if read_list == []:
+        return False
+    else:
+        return True
 
 
 robot_sender = 'robot2'
@@ -30,25 +30,25 @@ HOST = 'localhost'
 PORT = 4001
 
 if __name__ == '__main__':
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-		s.connect((HOST, PORT))
-		send(robot_sender, s)
-		start = ''
-		while 'Start' not in start:
-			start = receive(s)
-			print(start)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        send(robot_sender, s)
+        start = ''
+        while 'Start' not in start:
+            start = receive(s)
+            print(start)
 
-		message = robot_receiver + '{ciao}'
-		send(message, s)
+        message = robot_receiver + '{ciao}'
+        send(message, s)
 
-		while not messageInSocket(s):
-			pass
-		data = receive(s)
-		print(data)
+        while not messageInSocket(s):
+            pass
+        data = receive(s)
+        print(data)
 
-		while not messageInSocket(s):
-			pass
-		data = receive(s)
-		print(data)
+        while not messageInSocket(s):
+            pass
+        data = receive(s)
+        print(data)
 
-		s.close()
+        s.close()
