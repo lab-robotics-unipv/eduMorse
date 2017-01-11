2800efa3c4c6cc3e54a7329b052eefe2405e8d26
Andrea Bonandin <andrea.bonandin@gmail.com>
2017-01-11 15:54:13 +0100

    l.toml and tutorial.toml together

--- eduMorse/score/layer.py
+++ eduMorse/score/layer.py
@@ -6,6 +6,16 @@ import sys
 import time
 
 
+# control if a file exists
+def findFileInPath(filename, extension, paths):
+	for path in paths:
+		if os.path.exists(os.path.join(path, filename)):
+			return os.path.join(path, filename)
+		elif os.path.exists(os.path.join(path, filename + '.' + extension)):
+			return os.path.join(path, filename + '.' + extension)
+	raise FileNotFoundError('File ' + filename + ' not found')
+
+
 # find the path of a simulation starting from the simulation name
 def findSimuPath(simuName):
 	with open(os.path.join(MORSECONFIGPATH, 'config'), 'r') as config:
@@ -43,6 +53,8 @@ HOST = 'localhost'
 PORT = 50000
 PORTSCORE = 50001
 MORSECONFIGPATH = os.path.join(os.environ['HOME'], '.morse/')
+EDUMORSEPATH = os.environ.get("EDUMORSEPATH")
+GAMESPATH = os.path.join(EDUMORSEPATH, "games")
 
 if __name__ == '__main__':
 
@@ -53,46 +65,56 @@ if __name__ == '__main__':
 	if SIMUPATH == None:
 		raise Exception('Simulation name not found')
 
-	# connect to collision.py
+	#connect to collision.py
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 		s.connect((HOST, PORT))
 
 		# read layer configuration file
-		with open(os.path.join(SIMUPATH, "l.toml"), 'r') as lfile:
-			layer = toml.loads(lfile.read())
-			layer = layer.get('layer', {})
+		with open(os.path.join(SIMUPATH, "simulation.toml"), 'r') as simulation_file:
+			simulation = toml.loads(simulation_file.read())
 
-			# open a socket to talk with score.py
-			with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socketScore:
-				socketScore.bind((HOST, PORTSCORE))
-				socketScore.listen(1)
-				conn, addr = socketScore.accept()
+			# Check if rules file exists and load it
+			rules_name = simulation['simulation']['name']
+			try:
+				rules_name = findFileInPath(rules_name, 'toml', [SIMUPATH, GAMESPATH])
+			except:
+				raise
 
-				#print("Press ctrl+C to stop")
+			with open(rules_name) as rules_file:
+				rules = toml.loads(rules_file.read())
+				layer = rules['simulation'].get('score', [])
 
-				try:
-					while True:
-						if len(layer.get('score', [])) == 0:
-							time.sleep(1)
-							continue
+				# open a socket to talk with score.py
+				with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socketScore:
+					socketScore.bind((HOST, PORTSCORE))
+					socketScore.listen(1)
+					conn, addr = socketScore.accept()
 
-						while not messageInSocket(s):
-							pass
+					#print("Press ctrl+C to stop")
 
-						message = receive(s)
-						parts = message.split('.')
-						robot = parts[0]
-						obj = parts[1:]
+					try:
+						while True:
+							if len(layer) == 0:
+								time.sleep(1)
+								continue
 
-						score = 0
-						stop = False
-						for l in layer['score']:
-							for o in obj:
-								if o in l['obj']:
-									score += l['score']
-									stop |= l['stop']
-									send(robot, str(score), str(stop), conn)
-				except (KeyboardInterrupt, SystemExit):
-					s.close()
-					socketScore.close()
-					print("layer.py is shutting down")
+							while not messageInSocket(s):
+								pass
+
+							message = receive(s)
+							parts = message.split('.')
+							robot = parts[0]
+							obj = parts[1:]
+
+							score = 0
+							stop = False
+							for l in layer:
+								for o in obj:
+									if o in l['obj']:
+										score += l['score']
+										stop |= l['stop']
+										send(robot, str(score), str(stop), conn)
+					except (KeyboardInterrupt, SystemExit):
+						s.close()
+						socketScore.close()
+						print("layer.py is shutting down")
