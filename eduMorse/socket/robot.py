ebaf7e8e6f6e1edee4120e82a7aaca926f8ae7fe
Andrea Bonandin <andrea.bonandin@gmail.com>
2017-03-25 11:22:51 +0100

    functions from library

--- eduMorse/socket/robot.py
+++ eduMorse/socket/robot.py
@@ -1,34 +1,7 @@
-import json
 import os
-import select
 import socket
+import socketComm
 import time
-
-def receive(conn):
-    data = b''
-    word = b''
-    while word != b'\x04':
-        data += word
-        word = conn.recv(1)
-    message = data.decode('utf-8')
-    message = json.loads(message)
-    return message
-
-def send(msg, socket):
-    message = json.dumps(msg)
-    message = message + '\x04'
-    socket.sendall(message.encode('utf-8'))
-
-def messageInSocket(s):
-    read_list, _, _ = select.select([s], [], [], 0)
-    if read_list == []:
-        return False
-    else:
-        return True
-
-def stopRobot(robot, socket):
-    msg = {'CONTROLLER' : {robot : 'STOP'}}
-    send(msg, socket)
 
 robot_sender = 'robot'
 robot_receiver = 'robot2'
@@ -38,24 +11,26 @@ PORT = int(os.environ.get("EDUMORSE_ROBOT_PORT"))
 if __name__ == '__main__':
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        send(robot_sender, s)
+        socketComm.send(robot_sender, s)
         start = ''
         while 'Start' not in start:
-            start = receive(s)
+            start = socketComm.receive(s)
             print(start)
+
+        #socketComm.stopRobot(robot_sender, s)
 
         text = 'Hello from robot!'
         message = {robot_receiver : text}
-        send(message, s)
+        socketComm.send(message, s)
 
         time.sleep(0.6)
         text = 'Hello again!'
         message = {robot_receiver : text}
-        send(message, s)
+        socketComm.send(message, s)
 
-        while not messageInSocket(s):
+        while not socketComm.messageInSocket(s):
             pass
-        data = receive(s)
+        data = socketComm.receive(s)
         msg = data['robot']
         print(msg)
 
